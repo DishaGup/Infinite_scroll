9dd45f7f924f4987681f90c9fe6be98df4a30127
Disha Gupta <115460391+DishaGup@users.noreply.github.com>
2023-06-04 16:19:54 +0530

    data processing

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    babel: {
+        presets: ['next/babel'],
+        plugins: [['transform-react-jsx', { pragma: 'React.createElement' }]],
+      },
+}
 
 module.exports = nextConfig
